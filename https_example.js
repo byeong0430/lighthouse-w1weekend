a462fa115ffde9f6ab51c304003082bd3c917908
byeong.kim0430@gmail.com <byeong.kim0430@gmail.com>
2018-06-10 20:34:09 +0000

    Created a simple request

--- https_example.js
+++ https_example.js
@@ -1,3 +1,21 @@
 // Require Node.js https module
 const https = require('https');
 console.log('I did it!');
+
+// Create options Object
+// options syntax: https://nodejs.org/api/http.html#http_http_request_options_callback
+const options = {
+  host: 'www.example.org',
+  path: '/'
+};
+
+// Callback function, called by https when the request is made.
+const callback = function(){
+  console.log('In response handler callback!');
+};
+
+console.log("I'm about to make the request!");
+
+https.request(options, callback).end();
+
+console.log("I've made the request!");
