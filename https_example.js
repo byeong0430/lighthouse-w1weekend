5c9ea34db4b8efb64f99de2bc5060e6a5f289cbf
byeong.kim0430@gmail.com <byeong.kim0430@gmail.com>
2018-06-10 20:46:29 +0000

    Added readStream event handler in the callback function

--- https_example.js
+++ https_example.js
@@ -12,7 +12,22 @@ const options = {
 // Callback function, called by https when the request is made.
 const callback = function(response){
   console.log('In response handler callback!');
-  console.log('Response: ', response);
+  
+  // response.setEncoding('UTF8');
+  
+  // The response parameter is a stream! 
+  // Create a readStream event handler
+  response.on('data', function(chunk){
+    console.log(`-- CHUNK OF LENGTH ${chunk.length} --`);
+    
+    // chunk.toString() uses the encoding of UTF8
+    /* 
+      You can also specify the encoding 
+      by writing response.setEncoding('UTF8) outside response.on().
+      That way, you can just log chunk not chunk.toString()
+    */
+    console.log(chunk.toString());
+  });
 };
 
 console.log("I'm about to make the request!");
